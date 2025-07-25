b8af71bc234ccb51287a193e4388b194d42a6df5
chohc <hyej5501@naver.com>
2025-07-25 16:36:09 +0900

    새로운 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ function App() {
     <>
       <h1>배포 연습 중</h1>
       <h2>나는 이제 배포의 달인</h2>
-      <h2>나는 이제 배포의 달인</h2>
-      <h2>나는 이제 배포의 달인</h2>
+      Hello Vite-React! Hello Vite-React! Hello Vite-React! Hello Vite-React!
+      Hello Vite-React!
     </>
   );
 }
